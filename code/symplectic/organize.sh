0ea44c15f8e7984dc3657f797f7a9b4238de291b
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-12-09 11:46:16 -0500

    Feat: Automatization

---
+++ code/symplectic/organize.sh
@@ -0,0 +1,14 @@
+cd first; mkdir orbits; mkdir angular; mkdir energy;
+ mv *orbits.txt orbits
+ mv *angular.txt angular
+ mv *energy.txt energy
+ cd ..
+ cd second; mkdir orbits; mkdir angular; mkdir energy;
+ mv *orbits.txt orbits
+ mv *angular.txt angular
+ mv *energy.txt energy
+ cd ..
+ cd third; mkdir orbits; mkdir angular; mkdir energy;
+ mv *orbits.txt orbits
+ mv *angular.txt angular
+ mv *energy.txt energy
